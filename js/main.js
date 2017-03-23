7d031f34a6bb3af5cdd932941f0b0c479f730ea0
eric quach <thisiseq@gmail.com>
2017-03-23 15:29:00 -0700

    Update

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+
+$('#songimage1').on('click', function () {
+    // Type code related to event here! :)
+    $('#songinfo2').addClass('c1-tsi-2');
+    $('#songinfo3').addClass('c1-tsi-3');
+    $('#songinfo1').addClass('c1-tsi-1');
+    setTimeout(function () {
+    	$('#infoplayer1').addClass('c1-tip-1');
+    }, 550);
+
+ 
+    
+    
+});
+
+$('#return1').on('click', function () {
+    // Type code related to event here! :)
+   $('#infoplayer1').removeClass('c1-tip-1');
+   
+
+ setTimeout(function () {
+     $('#songinfo2').removeClass('c1-tsi-2');
+    $('#songinfo3').removeClass('c1-tsi-3');
+
+      }, 550);
+});
